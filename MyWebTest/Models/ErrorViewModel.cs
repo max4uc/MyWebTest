0facd6af0469e261a5eba6385bb26c808054deef
max4uc <max4uc@gmail.com>
2025-07-08 18:23:08 +0200

    Add project files.

---
+++ MyWebTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWebTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
